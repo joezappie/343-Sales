be637c1a25c0539bfe63a31921fc20e250e75589
Joe Campione <jtcamp96@gmail.com>
2017-03-23 17:20:16 -0400

    edits to customer api

--- routes/customer.js
+++ routes/customer.js
@@ -1,5 +1,12 @@
 var customer = function(req, res, next) {
-  res.json("hello");
+
+  var firstName = req.param('FirstName');
+  var lastName = req.param('LastName');
+  var email = req.param('Email');
+  var phoneNumber = req.param('PhoneNumber');
+  var customerId = req.param('CustomerId');
+
+  res.json({customers : [{customerId:customerId}]});
 };
 
 module.exports = customer;
