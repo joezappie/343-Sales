88987ee169f890385e9737ada49e58c3203fd08d
Dan Fisher <dfishe33@gmail.com>
2017-03-26 15:19:13 -0400

    Changed from a canned response to a kind of mock, the order endpoing now only returns stuff if the orderid exists in the mock set of data. Also set up endpoing for searching.

--- routes/api.js
+++ routes/api.js
@@ -17,15 +17,37 @@ router.get('/order',function(req,res,next) {
 	//int : Id of the order
 	var orderId = req.param('orderId');
 	//bool : if the billing info should be returned
-	var paymentInfo = req.param('paymentInfo');
+	var billingInfo = req.param('billingInfo');
+	billingInfo = billingInfo == 'true';
 	//bool : if the shipping info should be returned
 	var shippingInfo = req.param('shippingInfo');
+	shippingInfo = shippingInfo == 'true';
 	//bool : if the customer info should be returned
 	var customerInfo = req.param('customerInfo');
+	customerInfo = customerInfo == 'true';
 	//bool : if the list of items should be returned
-	var items = req.param('items');
+	var items = req.param('items');	
+	items = items == 'true';
+
+	orders.get(req,res,next,orderId,billingInfo,shippingInfo,customerInfo,items);	
+});
+
+/* GET search for order */
+router.get('/order/search',function(req,res,next){
+	//String : Shipping address
+	var address = req.param('address');
+	//bool : False by default, True will search by billing
+	var billingAddress = req.param('billingAddress');
+	//bool : if the billing info should be returned
+        var billingInfo = req.param('billingInfo');
+        //bool : if the shipping info should be returned
+        var shippingInfo = req.param('shippingInfo'); 
+        //bool : if the customer info should be returned
+        var customerInfo = req.param('customerInfo');
+        //bool : if the list of items should be returned
+        var items = req.param('items');
 	
-	orders(req,res,next,orderId,paymentInfo,shippingInfo,customerInfo,items);	
+	orders.query(req,res,next,address,billingAddress,billingInfo,shippingInfo,customerInfo,shippingInfo,customerInfo,items);
 });
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -1,70 +1,172 @@
-var orders = function(req,res,next,orderId,paymentInfo,shippingInfo,customerInfo,items){
-	var shippingInfoObj = {
-		"firstName": "john",
-		"lastName": "doe",
-		"address": "1111 street",
-		"zip": "14586",
-		"state": "NY"
-	};
-	var billingInfoObj = {
-		"firstName": "john",
-		"lastName": "doe",
-		"address": "1111 street",
-		"zip": "14586",
-		"state": "NY",
-		"ccLastFourDigets": "1234"
-	};
-	var customerInfoObj = {	
-		"customerId": 42,
-		"firstName": "John",
-		"lastName": "Johnson",
-		"email": "nottheotherjohn@email.com",
-		"phone": "5451112222"
-	};
-	var itemsAry = [
-		{
-			"serialId": 20,
-			"price": 100,
-			"status": "original|return|replace",
-			"replaceDeadline": "2017-03-01T20:51:26.908Z",
-			"refundDeadline": "2017-03-01T20:51:26.908Z"
+var orders = {};
+
+//This will be my mock database for 'searching', will be repleaced with the actual database
+var database = [
+	{
+		"id" : 1,
+		"customerId": 1,
+		"repId": 99,
+		"cost": 200,
+		"orderDate": "2017-03-01T20:51:26.905Z",
+		"isPaid": false,
+		"taxPercentage": 8,
+		"shippingInfo": {
+			"firstName": "john",
+			"lastName": "doe",
+			"address": "1111 street",
+			"zip": "14586",
+			"state": "NY"
 		},
-		{
-			"serialId": 21,
-			"price": 200,
-			"status": "original|return|replace",
-			"replaceDeadline": "2017-03-01T20:51:26.909Z",
-			"refundDeadline": "2017-03-01T20:51:26.909Z"
-		}
-	];
-
-	var orders = {
-		orders : [
-		{
-			id : orderId,
+		"billingInfo": {
+			"firstName": "john",
+			"lastName": "doe",
+			"address": "1111 street",
+			"zip": "14586",
+			"state": "NY",
+			"ccLastFourDigets": "1234"
+		},
+		"customerInfo": {
 			"customerId": 1,
-			"repId": 99,
-			"cost": 200,
-			"orderDate": "2017-03-01T20:51:26.905Z",
-			"isPaid": false,
-			"taxPercentage": 8,
-		}]	
-	};
+			"firstName": "John",
+			"lastName": "Johnson",
+			"email": "nottheotherjohn@email.com",
+			"phone": "5451112222"
+		},
+		"items": [
+			{
+				"serialId": 20,
+				"price": 100,
+				"status": "original|return|replace",
+				"replaceDeadline": "2017-03-01T20:51:26.908Z",
+				"refundDeadline": "2017-03-01T20:51:26.908Z"
+			},
+			{
+				"serialId": 21,
+				"price": 200,
+				"status": "original|return|replace",
+				"replaceDeadline": "2017-03-01T20:51:26.909Z",
+				"refundDeadline": "2017-03-01T20:51:26.909Z"
+			}
+		]
+	},
+	{
+		"id" : 2,
+		"customerId": 1,
+		"repId": 60,
+		"cost": 700.50,
+		"orderDate": "2017-03-20T20:51:26.905Z",
+		"isPaid": true,
+		"taxPercentage": 8,
+		"shippingInfo": {
+			"firstName": "john",
+			"lastName": "doe",
+			"address": "1111 street",
+			"zip": "14586",
+			"state": "NY"
+		},
+		"billingInfo": {
+			"firstName": "john",
+			"lastName": "doe",
+			"address": "1111 street",
+			"zip": "14586",
+			"state": "NY",
+			"ccLastFourDigets": "1234"
+		},
+		"customerInfo": {
+			"customerId": 1,
+			"firstName": "John",
+			"lastName": "Johnson",
+			"email": "nottheotherjohn@email.com",
+			"phone": "5451112222"
+		},
+		"items": [
+			{
+				"serialId": 12,
+				"price": 200.25,
+				"status": "original",
+				"replaceDeadline": "2017-04-20T20:51:26.908Z",
+				"refundDeadline": "2017-05-20T20:51:26.908Z"
+			},
+			{
+				"serialId": 22,
+				"price": 500.25,
+				"status": "original",
+				"replaceDeadline": "2017-04-20T20:51:26.909Z",
+				"refundDeadline": "2017-05-20T20:51:26.909Z"
+			}
+		]
+	},
+	{
+		"id" : 3,
+		"customerId": 2,
+		"repId": 12,
+		"cost": 100,
+		"orderDate": "2017-03-23T20:51:26.905Z",
+		"isPaid": true,
+		"taxPercentage": 8,
+		"shippingInfo": {
+			"firstName": "joe",
+			"lastName": "jefferson",
+			"address": "222 road",
+			"zip": "12345",
+			"state": "NY"
+		},
+		"billingInfo": {
+			"firstName": "joe",
+			"lastName": "jefferson",
+			"address": "222 road",
+			"zip": "12345",
+			"state": "NY",
+			"ccLastFourDigets": "1234"
+		},
+		"customerInfo": {
+			"customerId": 2,
+			"firstName": "Joe",
+			"lastName": "Jefferson",
+			"email": "therealjoe@joemail.com",
+			"phone": "1231231234"
+		},
+		"items": [
+			{
+				"serialId": 10,
+				"price": 100,
+				"status": "original",
+				"replaceDeadline": "2017-04-22T20:51:26.908Z",
+				"refundDeadline": "2017-05-22T20:51:26.908Z"
+			}	
+		]
+	}
+];
 
-	if(paymentInfo){
-		orders['orders'][0]['billingInfo'] = billingInfoObj;
-	}
-	if(shippingInfo){
-		orders['orders'][0]['shippingInfo'] = shippingInfoObj;
-	}
-	if(customerInfo){
-		orders['orders'][0]['customerInfo'] = customerInfoObj;
-	}
-	if(items){
-		orders['orders'][0]['items'] = itemsAry; 
-	}
+orders.get = function(req,res,next,orderId,billingInfo,shippingInfo,customerInfo,items){
+	console.log('billingInfo: '+ billingInfo + ', shippingInfo: ' + shippingInfo + ', customerInfo: ' + customerInfo + ', items: ' + items);
+
+	var orders = [];
+	database.forEach(function(data){
+		if(data['id'] == orderId){
+			//Deep clone the object because we will be removing elements
+			var order = JSON.parse(JSON.stringify(data));
+			if(!billingInfo || billingInfo == false){
+				delete order['billingInfo'];
+			}
+			if(!shippingInfo || shippingInfo == false){
+				delete order['shippingInfo'];
+			}
+			if(!customerInfo || customerInfo == false){
+				delete order['customerInfo'];
+			}
+			if(!items || items == false){
+				delete order['items']; 
+			}
+			orders.push(order);					
+		}
+	});	
 
 	res.json(orders);
+};
+
+orders.query = function(req,res,next,address,billingAddress,billingInfo,shippingInfo,customerInfo,shippingInfo,customerInfo,items){
+	
 };
 
 module.exports = orders;
