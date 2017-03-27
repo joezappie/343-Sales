31e115e7c1d7b508dd03a5955db3595d741db126
Cailin Li <cxl2467@rit.edu>
2017-03-26 23:37:20 -0400

    Add test utils

---
+++ test/testUtils.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+
+var expect = chai.expect;
+var should = chai.should();
+
+/**
+ * Verify the given object has the correct properties and property types
+ *
+ * obj - the given object
+ * properties - dictionary mapping property names to property types
+ */
+var verifyProperties = function(obj, properties) {
+	Object.keys(properties).forEach(function(propertyName) {
+		var propertyType = properties[propertyName];
+
+		obj.should.have.property(propertyName);
+		obj[propertyName].should.be.a(propertyType);
+	});
+};
+
+module.exports = {
+	verifyProperties
+};
