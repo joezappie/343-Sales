87eb6f80da89578f310a856fdee8694d69cc0ae6
Cailin Li <cxl2467@rit.edu>
2017-04-09 13:01:36 -0400

    Populate store page with inventory data

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 // Route requires
 router.use('/api', require('./api'));
 router.use('/consumed', require('./consumed'));
 
+var INVENTORY_BASE_URL = "http://vm343b.se.rit.edu:5000/inventory/";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('pages/index');
+	request(INVENTORY_BASE_URL + 'models/all', function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			//console.log(typeof(body));
+			var phoneModels = JSON.parse(body).map(function(model) {
+				return model[0];
+			});
+
+			res.render('pages/index', { phoneModels: phoneModels });
+		} else {
+			res.render('pages/index');
+		}
+	});
 });
 
 router.get('/apiTest', function(req, res) {
@@ -16,6 +30,10 @@ router.get('/apiTest', function(req, res) {
 
 router.get('/salesRep', function(req, res) {
 	res.render('pages/salesTest');
+});
+
+router.get('/shoppingCart', function(req, res) {
+	res.render('pages/shoppingCart');
 });
 
 module.exports = router;
