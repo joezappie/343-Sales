fa8751d1fb04b5f4918e210d2ab8e3bdda1dbb35
Justin Nietzel <Justin.A.Nietzel@gmail.com>
2017-04-10 21:21:24 -0400

    Pushing a working example of a class and instance methods for sequalize.

--- models/customer.js
+++ models/customer.js
@@ -13,5 +13,41 @@ module.exports = db.define('customer', {
     email: { type: Sequelize.STRING, allowNull: false},
     password: { type: Sequelize.STRING(32), allowNull: false},
     phoneNumber:  { type: Sequelize.STRING(20), allowNull: false},
-	isCompany: {type: Sequelize.BOOLEAN, allowNull: false},
-}, {timestamps: false, freezeTableName: true, tableName: 'Customer'});
+    isCompany: {type: Sequelize.BOOLEAN, allowNull: false},
+  },
+  //Start class settings
+  {timestamps: false, freezeTableName: true, tableName: 'Customer',
+
+  //Instance methods are technically part of the class settings.
+  //A method name maps to a define function.
+  instanceMethods : {
+    getFullname : function () {
+      return [this.firstName, this.lastName].join(' ');
+    }
+  },
+
+  //Here's an example of a class method. This cannot be called
+  //on instances of the customer method.
+  classMethods : {
+    test : function (){
+      return "test";
+    }
+  }
+});
+
+var CustomerClass = require("./customer.js");
+
+var customerInstance = CustomerClass.build(
+  {
+    id : 6,
+    firstName : "Jeff",
+    lastName : "Bridges",
+    email : "test@gmail.com",
+    password : "password",
+    phoneNumber : "555 234 4534",
+    isCompany : false
+  }
+);
+
+console.log(JSON.stringify(customerInstance.getFullname()));
+console.log(JSON.stringify(CustomerClass.test()));
