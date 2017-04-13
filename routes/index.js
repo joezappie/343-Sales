f8304662991c9538393dde794aa054babd15f9a5
Joe <jrj2211@rit.edu>
2017-04-12 23:39:46 -0400

    #42 - Basic implementation for creating customers with an address and payment method. Updated new customer page to have all of the address form input. Still needs validation on CC and address

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var request = require('request');
 var models = require(__base + 'models.js');
+var validator = require('validator');
 var router = express.Router();
 
 // Route requires
@@ -56,8 +57,89 @@ router.get('/shoppingCart', function(req, res) {
 	res.render('pages/shoppingCart');
 });
 
-router.get('/newCustomer', function(req, res) {
-	res.render('pages/newCustomer');
+router.all('/newCustomer', function(req, res) {
+	models.TaxRates.findAll().then(function(states) {
+		if(req.method == 'POST') {
+			var errors = [];
+			
+			if(req.body.lastName.length < 2) {
+				errors.push({'input': 'lastName', 'msg':'To short'});
+			} else if(req.body.lastName.length > 50) {
+				errors.push({'input': 'lastName', 'msg':'To long'});
+			}
+			
+			if(req.body.firstName.length < 2) {
+				errors.push({'input': 'firstName', 'msg':'To short'});
+			} else if(req.body.firstName.length > 50) {
+				errors.push({'input': 'firstName', 'msg':'To long'});
+			}
+			
+			if(!validator.isEmail(req.body.email)) {
+				errors.push({'input': 'email', 'msg':'Invalid email'});
+			}
+			
+			var patt = new RegExp(/^\+?1?\s*?\(?\d{3}(?:\)|[-|\s])?\s*?\d{3}[-|\s]?\d{4}$/);
+			if(!patt.test(req.body.phone)) {
+				errors.push({'input': 'phone', 'msg':'Invalid phone number'});
+			}
+			
+			
+			if(errors.length == 0) {
+				// Validated so create the objects
+				
+				var customerData = {
+					password: "",
+					phoneNumber: req.body.phone,
+					email: req.body.email,
+					firstName: req.body.firstName,
+					lastName: req.body.lastName,
+					isCompany: true,
+				}
+
+				models.Customer.create(customerData).then(function(customer) {
+					var addressData = {
+						address: req.body.address,
+						city: req.body.city,
+						zip: req.body.zipcode,
+						stateId: req.body.state,
+						customerId: customer.id,
+						firstName: customer.firstName,
+						lastName: customer.lastName,
+					}
+					
+			
+					models.Address.create(addressData).then(function(address) {
+						
+						var expirationDate = new Date(req.body.expiration_date);
+						
+						var paymentData = {
+							cardNumber: req.body.card_number,
+							CVC: req.body.cvc,
+							expirationDate: expirationDate.getTime() / 1000,
+							billingAddressId: address.id,
+						}
+						
+						models.PaymentMethod.create(paymentData).then(function(payment) {
+							res.render('pages/newCustomer', {states: states, errors: errors});
+						}).catch(function(err) {
+							res.json(err);
+						});
+					}).catch(function(err) {
+						res.json(err);
+					});
+					
+				}).catch(function(err) {
+					res.json(err);
+				});
+			
+			} else {
+				res.render('pages/newCustomer', {states: states, errors: errors});
+			}
+		} else {
+			res.render('pages/newCustomer', {states: states});
+		}
+		
+	});
 });
 
 module.exports = router;
